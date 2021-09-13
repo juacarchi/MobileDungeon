7070d9d1fe2d7aca0c1e8a974083cecf08db9841
juacarchi <cardenasfanoce@gmail.com>
2021-09-13 21:18:19 +0200

    NewProject

---
+++ MobileDungeon/Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+
+public class TopDownCharacterController : MonoBehaviour
+{
+    public float speed;
+    [SerializeField]
+    Joystick joystickMovement;
+    Rigidbody2D rb2D;
+    Vector2 move;
+    SpriteRenderer rendererPlayer;
+    [SerializeField]
+    GameObject weapon;
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rendererPlayer = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        move.x = joystickMovement.Horizontal;
+        move.y = joystickMovement.Vertical;
+
+        if (move.x < 0)
+        {
+            rendererPlayer.flipX = true;
+            weapon.transform.localScale = new Vector2(1, -1);
+        }
+        else if (move.x > 0)
+        {
+            rendererPlayer.flipX = false;
+            weapon.transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            if (rendererPlayer.flipX)
+            {
+                weapon.transform.localScale = new Vector2(-1, 1);
+            }
+            else
+            {
+                weapon.transform.localScale = new Vector2(1, 1);
+            }
+
+        }
+    }
+    private void FixedUpdate()
+    {
+        weapon.transform.right = move;
+        rb2D.MovePosition(rb2D.position + move * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ MobileDungeon/Assets/Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FloatingJoystick : Joystick
+{
+    Vector2 anchoredPosInitial;
+    protected override void Start()
+    {
+        base.Start();
+        background.gameObject.SetActive(true);
+        anchoredPosInitial = background.anchoredPosition;
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
+        
+        base.OnPointerDown(eventData);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        background.anchoredPosition = anchoredPosInitial;
+        base.OnPointerUp(eventData);
+    }
+}
